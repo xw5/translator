3b58d0264c31bfea8b4196865e44e9526f29af97
xiewu <xiewu@ygsoft.com>
2021-04-29 15:41:10 +0800

    完善插件提交

--- core/translate.js
+++ core/translate.js
@@ -1,20 +1,71 @@
 let axios = require("axios");
 var pinyin = require("chinese-to-pinyin");
+
 /**
  * 执行翻译
  * @param {String} keywords
  */
-function translateAction(keywords) {
+function translateByYoudao(keywords) {
 	if (!keywords) {
 		return;
 	}
-	return axios.get('https://fanyi.youdao.com/translate', {
-		params: {
-			doctype: "json",
-			type: "AUTO",
-			i: keywords
-		}
-	});
+  return new Promise((resolve, reject) => {
+    axios.get('https://fanyi.youdao.com/translate', {
+    	params: {
+    		doctype: "json",
+    		type: "AUTO",
+    		i: keywords
+    	}
+    }).then((response) => {
+      let result = response.data;
+      if (result.errorCode === 0) {
+        resolve({
+          errorCode: result.errorCode,
+          value: result['translateResult'][0][0].tgt
+        })
+      } else {
+        resolve({
+          errorCode: result.errorCode,
+          value: null
+        })
+      }
+    }).catch((err) => {
+      reject(err);
+    });
+  })
+}
+
+/**
+ * 执行翻译
+ * @param {String} keywords
+ */
+function translateByGoogle(keywords) {
+	if (!keywords) {
+		return;
+	}
+  return new Promise((resolve, reject) => {
+    axios.get('http://translate.google.cn/translate_a/single?client=gtx&dt=t&dj=1&ie=UTF-8&sl=auto', {
+      params: {
+        q: keywords,
+        tl: /[\u4e00-\u9fa5]/.test(keywords) ? 'en' : 'zh'
+      }
+    }).then((response) => {
+      let result = response.data;
+      if (result.sentences && result.sentences.length > 0) {
+        resolve({
+          errorCode: 0,
+          value: result['sentences'][0].trans
+        })
+      } else {
+        resolve({
+          errorCode: -1,
+          value: null
+        })
+      }
+    }).catch((err) => {
+      reject(err);
+    });
+  })
 }
 
 /**
@@ -26,6 +77,11 @@ function translatePyAction(keywords) {
 		return;
 	}
 	return pinyin(keywords, { keepRest: true, removeTone: true});
+}
+
+const translateAction = {
+  google: translateByGoogle,
+  youdao: translateByYoudao
 }
 
 module.exports = {
--- extension.js
+++ extension.js
@@ -2,11 +2,6 @@ var hx = require("hbuilderx");
 var showWebView = require('./core/webview.js');
 //该方法将在插件激活的时候调用
 function activate(context) {
-	// let disposable = hx.commands.registerCommand('extension.helloWorld', () => {
-	// 	hx.window.showInformationMessage('你好，这是我的第一个插件扩展。');
-	// });
-	// //订阅销毁钩子，插件禁用的时候，自动注销该command。
-	// context.subscriptions.push(disposable);
 	
 	let webviewPanelActivityAbr = hx.window.createWebView("api.WebViewActivityBar", {
 	    enableScritps: true
